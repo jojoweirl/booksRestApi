6c8ec17df4a399495dbd2316cc0e813a9468c90c
Fabrice <fesope@ironflytechnologies.com>
2021-06-29 23:21:12 +0800

    mov

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.heroku...]
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 //routes
 app.use("/api/books", booksRoute);
 
@@ -24,6 +37,13 @@ mongoose
   .catch((error) => {
     console.log("Error connecting mongoose", error);
   });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("App started");
